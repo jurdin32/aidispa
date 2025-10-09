e18e4889742cd23b10b04d02310f06b11cbbe146
Hartman2003 <halvareza@uees.edu.ec>
2025-10-09 00:05:16 -0500

    cambios iniciales

--- pull_git.sh
+++ pull_git.sh
@@ -1,6 +1,5 @@
 cd /var/www/html/aidispa && \
 git add . && \
-git commit -m "Guardando cambios locales antes de pull" && \
-git config pull.rebase false && \
+git commit -m "Guardando cambios locales antes de pull" || true && \
 git pull --no-rebase origin main && \
 sudo systemctl restart apache2
